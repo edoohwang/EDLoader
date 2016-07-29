bf97b0ea30d233744bce0fb3bed5ba83f2ce5453
edoohwang <edoohwang@gmail.com>
2016-07-29 11:42:45 +0800

    0.0.4 Ver.

---
+++ EDLoader/EDLoader/EDLoader.h
@@ -0,0 +1,24 @@
+//
+//  EDLoader.h
+//  EDLoader
+//
+//  Created by edoohwang on 7/29/16.
+//  Copyright © 2016 edoohwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "EDLoader.swift"
+#import "EDLoaderCommon.swift"
+#import "NSBundle+EDLoader.swift"
+#import "UIScrollView+EDLoader.swift"
+#import "UIView+EDLoader.swift"
+
+//! Project version number for EDLoader.
+FOUNDATION_EXPORT double EDLoaderVersionNumber;
+
+//! Project version string for EDLoader.
+FOUNDATION_EXPORT const unsigned char EDLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EDLoader/PublicHeader.h>
+
+
